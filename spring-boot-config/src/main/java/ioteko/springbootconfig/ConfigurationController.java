3f13d688c5987c02bede62271a7d771dfd7cccf0
4teko7 <bilal.tekin@boun.edu.tr>
2020-04-23 22:22:22 +0300

    AfterUpdatingConfigFileAutoRefreshingWithTheHelpOfPostmanWasAdded

--- spring-boot-config/src/main/java/ioteko/springbootconfig/ConfigurationController.java
+++ spring-boot-config/src/main/java/ioteko/springbootconfig/ConfigurationController.java
@@ -2,12 +2,14 @@ package ioteko.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/configuration")
+@RefreshScope
 public class ConfigurationController {
 
 	@Autowired
